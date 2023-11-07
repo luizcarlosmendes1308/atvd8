345261310c0cc1b5ce1b09ad47e1f9c06cdaba8f
Luiz carlos mendes <82839686+luizcarlosmendes1308@users.noreply.github.com>
2023-11-07 11:35:14 -0300

    Add files via upload

---
+++ ED1I4.Atividade8/Atividade8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade8
+{
+    /*
+* PARTICIPANTES
+* Luiz Carlos Mendes 3012123
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cadastro = new Cadastro();
+            var seletor = new Seletor(cadastro);
+
+            int opcao = -1;
+            while (opcao != 0)
+            {
+                opcao = seletor.EscolherOpcao();
+            }
+        }
+    }
+}
